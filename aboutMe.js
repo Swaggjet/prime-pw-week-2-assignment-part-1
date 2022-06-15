450450530dc7a3fcb3ecafb5aae982dc9e572479
Swaggjet <asantesimmons@gmail.com>
2022-06-15 11:40:30 -0500

    week2pt1

---
+++ aboutMe.js
@@ -0,0 +1,18 @@
+onsole.log('js');
+
+
+// REQUIRED FEATURES:
+console.log('js');
+// REQUIRED FEATURES:
+// 1 - Create a variable called `firstName` and assign it the value of your first name
+let firstName = 'Asante ';
+// 2 - Create a second variable called `lastName` and assign it the value of your last name
+let lastName = 'Simmons';
+// 3 - Create a third variable called `fullName`, assign it the value of your first and last name
+// (remember, you already have variables for this, can you use those?)
+let fullName = firstName + lastName ;
+// 4 - Console log the value of `fullName`
+console.log( fullName ); // Asante Simmons
+
+
+
